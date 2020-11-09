7459e0f7a6aa1b93bb78a8715775ca27ff2cd3ce
rhianeKobar <shakira.r.salazar@gmail.com>
2020-11-09 12:37:11 +0000

    trying to open the damn pokeball

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const button = document.getElementById('button2');
+//variables
+const button = document.getElementById('button');
 const ball = document.getElementById('ball');
-function shake(){
+const pokeballGif = document.getElementById('openBall');
+
+//functions
+const loading = () =>{
   ball.classList.add("shaking");
-  button.innerHTML= "fuck me";
+  button.classList.add("flashing");
 } 
-button.addEventListener("click", shake, true); 
+const levelOut = () =>{
+  ball.classList.add("levelOut");
+}
+const unLevel = () => {
+  ball.classList.add("unLevel");
+}
+const clicked = () => {
+  pokeballGif.style.visibility = visible;
+  pokeballGif.style.display= relative;
+}
+const 2clicked = () => {
+  pokeballGif.style.visibility = hidden;
+  pokeballGif.style.display= none;
+}
+//event handlers
+window.onload = loading;
+button.addEventListener("click",clicked);
+button.removeEventListener("click",2clicked);
+//ball.addEventListener("mouseenter", levelOut);
+//ball.addEventListener("mouseleave", unLevel);
 
 
